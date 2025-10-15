d0263e72c1e1cb5ba5ad0339f375466d42aaeb18
spinpa <92831339+spinpar@users.noreply.github.com>
2025-10-15 01:09:00 -0500

    Style

--- weebrary/settings.py
+++ weebrary/settings.py
@@ -107,6 +107,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / "STYLES" / "static",
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
