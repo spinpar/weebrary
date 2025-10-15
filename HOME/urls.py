26638a4a19adbc7ded23dfa0c95bc53560137d27
spinpa <92831339+spinpar@users.noreply.github.com>
2025-10-14 23:58:28 -0500

    API TESTING

--- HOME/urls.py
+++ HOME/urls.py
@@ -5,4 +5,5 @@ from . import views
 
 urlpatterns = [
     path("", views.homepage, name="homepage"),
+#    path("button/", views.api_button, name="button_action"),
 ]
--- HOME/views.py
+++ HOME/views.py
@@ -1,4 +1,12 @@
+import requests
 from django.shortcuts import render
 
 def homepage(request):
-    return render(request, 'home.html')
+    data = None
+    if request.method == "POST":
+        response = requests.get("https://kitsu.io/api/edge/anime?filter[text]=death")
+        if response.status_code == 200:
+            data = response.json()
+        else:
+            data = {"error": f"API returned {response.status_code}"}
+    return render(request, "home.html", {"data": data})
